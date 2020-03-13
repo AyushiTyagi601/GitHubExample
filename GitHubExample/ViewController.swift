af762b64e010817a4e8c73316ed990f79a1da46f
Ayushi Tyagi <ayushityagi601@gmail.com>
2020-03-13 16:03:06 +0530

    deleted operation

---
+++ GitHubExample/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitHubExample
+//
+//  Created by Vishal Madheshiya on 13/03/20.
+//  Copyright © 2020 Vishal Madheshiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("update some code")
+        print("new deleted operation")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
